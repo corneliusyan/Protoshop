c77474a156cc76cea83836241a9c313576816866
corneliusyan <onel.raja@gmail.com>
2019-11-20 00:03:01 +0700

    Initiate corner warp

--- src/anpr/corner_warp.cpp
+++ src/anpr/corner_warp.cpp
@@ -1,7 +1,35 @@
 #include "../image/image.hpp"
 #include "./t_corner.hpp"
 #include "./corner_warp.hpp"
+#include <cmath>
+#include <algorithm>
 
-Image* warpByCorner(Image* img, t_corner corner) {
+
+Image* warpByCorner(Image* image_src, t_corner src) {
+  int widthA, widthB, maxWidth, heightA, heightB, maxHeight;
+  widthA = round(sqrt(((corner.br.r - corner.bl.r) ^ 2) + ((corner.br.c - corner.bl.c) ^ 2)));
+  widthA = round(sqrt(((corner.tr.r - corner.tl.r) ^ 2) + ((corner.tr.c - corner.tl.c) ^ 2)));
+  maxWidth = std::max(widthA,widthB);
+
+  heightA = round(sqrt(((corner.tr.r - corner.br.r) ^ 2) + ((corner.tr.c - corner.br.c) ^ 2)));
+  heightB = round(sqrt(((corner.tl.r - corner.bl.r) ^ 2) + ((corner.tl.c - corner.bl.c) ^ 2)));
+  maxHeight = std::max(heightA,heightB);
+
+  Image* img = new Image(image_src->imageType, maxHeight, maxWidth);
+  int N = img->height, M = img->width;
+
+  t_point tl(0,0), tr(maxWidth-1, 0), br(maxWidth-1, maxHeight-1), bl(0, maxHeight-1);
+  t_corner dst(tl,tr,br,bl);
+
+  /*
+  Todo:
+  1. getPerspectiveTransform
+  2. Solve Matrix
+  3. warpPerspective -> kali aja
+  4. Handle non integer
+  5. Convert to binary
+  */
+
+
   return NULL;
 }
--- src/anpr/corner_warp.hpp
+++ src/anpr/corner_warp.hpp
@@ -4,7 +4,7 @@
 #include "../image/image.hpp"
 #include "./t_corner.hpp"
 
-Image* warpByCorner(Image* img, t_corner corner);
+Image* warpByCorner(Image* image_src, t_corner src);
 // create a new image by wrapping with corner as new corner
 
 #endif
