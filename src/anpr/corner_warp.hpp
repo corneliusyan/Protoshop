e7a7dd128096f1db323789cae180bb72f6bcf84c
corneliusyan <onel.raja@gmail.com>
2019-11-20 07:23:15 +0700

    [Checkpoint] getPerspectiveTransform

--- src/anpr/corner_warp.hpp
+++ src/anpr/corner_warp.hpp
@@ -4,6 +4,13 @@
 #include "../image/image.hpp"
 #include "./t_corner.hpp"
 
+struct Point {
+  int x;
+  int y;
+};
+
+Point* tcornerToPoint(t_corner _src);
+
 Image* warpByCorner(Image* image_src, t_corner src);
 // create a new image by wrapping with corner as new corner
 
